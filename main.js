f91faf2eb5962678fda1658532b5388e4f8d68a0
Kim Sarabia <kimberly.sarabia@gmail.com>
2016-04-14 20:02:35 -0700

    not much progress

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
 'use strict';
+
+
+$(document).ready(init);
+
+function init(){
+  console.log('ready!');
+  $('#cardHolder').on('click', '.card', clickCard);
+  $('#addCards').click(addCards);
+
+}
+
+function addCards(){
+  console.log('add:','add works');
+  var numCards = $('#numCards').val();
+  var $cards = [];
+
+  for(var i = 0; i < numCards; i++){
+    var $card = $('<div>').addClass('card');
+    $card.text(); // how to grab info from card?
+    $card.push()
+  }
+}
+
+function clickCard(event){
+  debugger;
+}
