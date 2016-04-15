38a5dd210ae383f1e2c5ef4eca4732b1368e6577
Kim Sarabia <kimberly.sarabia@gmail.com>
2016-04-15 08:55:09 -0700

    redesigned UI to bootstrap and finished up logic

--- main.js
+++ main.js
@@ -2,47 +2,96 @@
 
 $(document).ready(init);
 
-var woes = [];
-var changeWoe = false;
-var isAlpha = false;
-var isPhone = false;
-var isAlphaEmail = false;
-var isBirth = false;
-var editObj;
+var contacts = [];
 
 function init() {
   updateList();
   var $body = $('#body');
 
   $('#add').click(add);
-  $body.on('click', '.remove', remove);
-  $body.on('click', '.edit', edit);
   $body.on('click', '#editConfirm', confirm);
-  $body.on('click', '.select',(select));
-  $('#removeSelected').click(removeSelected);
   $('#sortAlpha').click(sortAlpha);
   $('#sortAlphaEmail').click(sortAlphaEmail);
   $('#sortBirth').click(sortBirth);
-  $('#revealAll').click(revealAll);
-  $('#revealFriends').click(revealFriends);
-  $('#revealFamily').click(revealFamily);
-  $('#revealCustom').click(revealCustom);
+  $('#getAll').click(getAll);
+  $('#getWoes').click(getWoes);
+  $('#getFam').click(getFam);
+  $('#showCustom').click(showCustom);
 }
 
 function add() {
-  console.log('add',add);
   var $newNameVal = $('#newName').val();
   var $newPhoneVal = $('#newPhone').val();
   var $newEmailVal = $('#newEmail').val();
   if ( (!$newNameVal) && (!$newPhoneVal && !$newEmailVal)) {
-    alert("At least enter a name, phone number, or email.");
+    alert("Please enter your name, email, and number.");
     return;
   };
-  var woe = {};
-  woe.name = $newNameVal;
-  woe.phone = $newPhoneVal;
-  woe.email = $newEmailVal;
-  woe.group = _.uniq($('#newGroup').val().toLowerCase().split(/\W/)) ;
-  woe.birthday = $('#newBirthday').val();
-  woes.push(woe);
+  var contact = {};
+  contact.name = $newNameVal;
+  contact.phone = $newPhoneVal;
+  contact.email = $newEmailVal;
+  contact.group = _.uniq($('#newGroup').val().toLowerCase().split(/\W/)) ;
+  contact.birthday = $('#newBirthday').val();
+  contacts.push(contact);
+
+  updateList();
 }
+
+function updateList(){
+  var $contactList = $('#body');
+  $contactList.empty();
+  var $contacts = $('<div>').addClass('container').attr('id', "body");
+  contacts.forEach(function(contact){
+    var $newRow = $('<div>').addClass('row item');
+    $newRow.append($('<div>').addClass('col-sm-3 name').text(contact.name) );
+    $newRow.append($('<div>').addClass('col-sm-1 phone').text(contact.phone) );
+    $newRow.append($('<div>').addClass('col-sm-2 email').text(contact.email) );
+    $newRow.append($('<div>').addClass('col-sm-2 group').text(contact.group) );
+    $newRow.append($('<div>').addClass('col-sm-2 birthday').text(contact.birthday) );
+    var $icons = $('<div>').addClass('col-sm-2 row');
+    $icons.append($('<input />', { type: 'url'}).addClass('select col-sm-4'));
+    $newRow.append($icons);
+    $contacts.append($newRow);
+  });
+  $contactList.append($contacts);
+  $("#total").text(contacts.length);
+}
+
+function getAll(){
+  $('.item').removeClass('hide');
+};
+
+function getWoes(){
+  var $item = $('.item');
+  $item.addClass('hide');
+  $item.each(function(index){
+    if ( _.includes($item.eq(index).children(".group").text().split(/\W/), 'friends') ) {
+      $item.eq(index).removeClass('hide');
+    }
+  });
+};
+
+function getFam(){
+  var $item = $('.item');
+  $item.addClass('hide');
+  $item.each(function(index){
+    if ( _.includes($item.eq(index).children(".group").text().split(/\W/), 'family') ) {
+      $item.eq(index).removeClass('hide');
+    }
+  });
+};
+
+function showCustom(){
+  var $customFilter = $('#customFilter');
+  var array = $customFilter.val().toLowerCase().split(/\W/);
+  var $item = $('.item');
+  $item.addClass('hide');
+  $item.each(function(index){
+    array.forEach(function(entry){
+      if ( _.includes($item.eq(index).children(".group").text().split(/\W/), entry) ) {
+        $item.eq(index).removeClass('hide');
+      }
+    });
+  });
+};
