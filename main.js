e5fbe92b5a76e689183618a8bae09e310da194ce
Kim Sarabia <kimberly.sarabia@gmail.com>
2016-04-16 08:50:13 -0700

    went back to bootstrap

--- main.js
+++ main.js
@@ -4,7 +4,10 @@ $(document).ready(init);
 
 var contacts = [];
 var editingContact = false;
-
+var isAlpha = false;
+var isPhone = false;
+var isAlphaEmail = false;
+var isBirth = false;
 var editObj;
 
 function init() {
@@ -12,13 +15,15 @@ function init() {
   updateList();
   var $list = $('#list');
 
-  $('#submit').click(add);
+  $('#add').click(add);
   $list.on('click', '.remove', remove);
   $list.on('click', '.edit', edit);
   $list.on('click', '#editConfirm', confirm);
   $list.on('click', '.select',(select));
   $('#removeSelected').click(removeSelected);
-
+  $('#sortAlpha').click(sortAlpha);
+  $('#sortAlphaEmail').click(sortAlphaEmail);
+  $('#sortBirth').click(sortBirth);
   $('#showAll').click(showAll);
   $('#showFriends').click(showFriends);
   $('#showFamily').click(showFamily);
@@ -27,19 +32,18 @@ function init() {
 
 
 function add() {
-  var $newNameVal = $('#first_name').val();
-  var $newPhoneVal = $('#icon_telephone').val();
-  var $newEmailVal = $('#email').val();
-  if ( (!$newNameVal) && (!$newPhoneVal && !$newEmailVal)) {
+  var $newNameVal = $('#newName').val();
+  var $addPhoneVal = $('#addPhone').val();
+  var $addEmailVal = $('#addEmail').val();
+  if ( (!$newNameVal) && (!$addPhoneVal && !$addEmailVal)) {
     alert("At least enter a name, phone number, or email.");
     return;
   };
   var contact = {};
   contact.name = $newNameVal;
-  contact.phone = $newPhoneVal;
-  contact.email = $newEmailVal;
-  contact.group = _.uniq($('#newGroup').val().toLowerCase().split(/\W/)) ;
-  contact.birthday = $('.datepicker').val();
+  contact.phone = $addPhoneVal;
+  contact.email = $addEmailVal;
+  contact.birthday = $('#addBirthday').val();
   contacts.push(contact);
 
   saveToStorage();
@@ -60,7 +64,7 @@ function loadFromStorage(){
 function updateList(){
   var $contactList = $('#list');
   $contactList.empty();
-  var $contacts = $('<div>').addClass('list').attr('contacts-table', "list");
+  var $contacts = $('<div>').addClass('container').attr('id', "list");
   contacts.forEach(function(contact){
     var $newRow = $('<div>').addClass('row item');
     $newRow.append($('<div>').addClass('col-sm-3 name').text(contact.name) );
@@ -68,7 +72,12 @@ function updateList(){
     $newRow.append($('<div>').addClass('col-sm-2 email').text(contact.email) );
     $newRow.append($('<div>').addClass('col-sm-2 group').text(contact.group) );
     $newRow.append($('<div>').addClass('col-sm-2 birthday').text(contact.birthday) );
-
+    var $icons = $('<div>').addClass('col-sm-2 row');
+    $icons.append($('<span>').addClass('remove col-sm-4').text('\u27F0'));
+    $icons.append($('<input />', { type: 'checkbox'}).addClass('select col-sm-4'));
+    $icons.append($('<span>').addClass('edit col-sm-4').text('\u270E'));
+    $newRow.append($icons);
+    $contacts.append($newRow);
   });
   $contactList.append($contacts);
   $("#total").text(contacts.length);
@@ -76,7 +85,10 @@ function updateList(){
 
 
 function remove(){
-  if (editingContact) { alert("Finish editing your contact or close the edit form"); return; };
+  if (editingContact) {
+    sweetAlert("Oops...", "Please finish editing or close the window!", "error");
+    return;
+  };
   var index = $(this).closest('.item').index();
   contacts.splice(index, 1);
   updateList();
@@ -90,7 +102,11 @@ function select(){
 };
 
 function removeSelected(){
-  if (editingContact) { alert("Finish editing your contact or close the edit form"); return; };
+  if (editingContact) {
+    sweetAlert("Oops...", "Please finish editing or close the window!", "error");
+    return;
+  };
+
   var $item = $('.item');
   var indexes = [];
   $item.each(function(index){
@@ -105,18 +121,81 @@ function removeSelected(){
   saveToStorage();
 };
 
+function edit(){
+  var $this = $(this);
+  var $parent = $this.closest('.item');
+  var index = $parent.index();
+  if ($parent.hasClass("isEditing")) {
+    closeEditForm();
+  } else if(!editingContact) {
+    editingContact = true;
+    editObj = {};
+    $parent.addClass("isEditing");
+    $parent.attr("id", "previous");
+    $parent.children(".name").attr("id", "previousName");
+    $parent.children(".phone").attr("id", "previousPhone");
+    $parent.children(".email").attr("id", "previousEmail");
+    $parent.children(".birthday").attr("id", "previousBirthday");
+    $parent.children(".group").attr("id", "previousGroup");
+    editObj.name = $("#previousName").text();
+    editObj.phone = $("#previousPhone").text();
+    editObj.email = $("#previousEmail").text();
+    editObj.birthday = $("#previousBirthday").text();
+    editObj.group = $("#previousGroup").text().split(/\W/);
 
+    var $editForm = $('<div>').addClass('row editing').attr("id", "editForm");
+    $editForm.append($('<span>').addClass('col-sm-1').text('Name:'));
+    $editForm.append($('<input>').addClass('col-sm-4').attr({type: "text", id: "editName", value: editObj.name} ) );
+
+    $editForm.append($('<span>').addClass('col-sm-1').text('Phone:'));
+    $editForm.append($('<input>').addClass('col-sm-2').attr({type: "number", id: "editPhone", step: "1", min: "0", value: editObj.phone} ) );
+
+    $editForm.append($('<span>').addClass('col-sm-1').text('Email:'));
+    $editForm.append($('<input>').addClass('col-sm-3').attr({type: "text", id: "editEmail", value: editObj.email} ) );
+
+    var $editForm2 = $('<div>').addClass('row editing').attr("id", "editForm2");
+    $editForm2.append($('<span>').addClass('col-sm-1').text('Groups:'));
+    $editForm2.append($('<input>').addClass('col-sm-5').attr({type: "text", id: "editGroup", value: editObj.group} ) );
+
+    $editForm2.append($('<span>').addClass('col-sm-2').text('Birthday:'));
+    $editForm2.append($('<input>').addClass('col-sm-3').attr({type: "date", id: "editBirthday", value: editObj.birthday} ) );
+
+    $editForm2.append($('<div>').addClass('col-sm-1 btn btn-default').text('Confirm').attr("id", "editConfirm"));
+    $parent.after($editForm, $editForm2);
+  }
+
+
+};
+
+function confirm(){
+  editObj.name = $('#editName').val();
+  editObj.phone = $('#editPhone').val();
+  editObj.email = $('#editEmail').val();
+  editObj.birthday = $('#editBirthday').val();
+  editObj.group = _.uniq( $('#editGroup').val().toLowerCase().split(/\W/) );
+  var index = $('#previous').index();
+  contacts[index].name = editObj.name;
+  contacts[index].phone = editObj.phone;
+  contacts[index].email = editObj.email;
+  contacts[index].birthday = editObj.birthday;
+  contacts[index].group = editObj.group;
+  closeEditForm();
+  updateList();
+  saveToStorage();
+};
+
+function closeEditForm(){
+  editObj = {};
+  var $previous = $("#previous");
+  $previous.children(".type").removeAttr("id");
+  $previous.children(".amount").removeAttr("id");
+  $previous.removeClass("isEditing");
+  $previous.removeAttr("id");
+  $("#editForm").remove();
+  $("#editForm2").remove();
+  editingContact = false;
+};
 
 function showAll(){
   $('.item').removeClass('hide');
-};
-
-function showFriends(){
-  var $item = $('.item');
-  $item.addClass('hide');
-  $item.each(function(index){
-    if ( _.includes($item.eq(index).children(".group").text().split(/\W/), 'friends') ) {
-      $item.eq(index).removeClass('hide');
-    }
-  });
 };
